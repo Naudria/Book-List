d8e249dff725091cad18f13f292c64b860c11c23
Naudria <rima.jean@gmail.com>
2019-01-11 12:11:35 -0600

    Make relationships a bit more complex by adding migration to create Genres table with one column, Name.

--- db/migrate/20190108224809_create_books.rb
+++ db/migrate/20190108224809_create_books.rb
@@ -3,7 +3,7 @@ class CreateBooks < ActiveRecord::Migration[5.2]
   	create_table :books do |t|
   		t.string :title
   		t.string :author
-  		t.string :genre
+  		t.integer :genre_id
         t.integer :user_id
   	end
   end
